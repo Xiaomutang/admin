d364453dd8bc050838210878b9a200fac775e109
wangdong <1029507088@qq.com>
2018-07-08 20:32:41 +0800

    用户表数据展示

--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -1,15 +1,86 @@
 <template>
-    <div>
-        users
-    </div>
+  <el-card class="box-card">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-row class="searchArea">
+      <el-col :span="24">
+        <el-input class="searchInput" clearable placeholder="请输入内容">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      :data="list"
+      stripe
+      border
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="创建日期">
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const token = sessionStorage.getItem('token');
+      this.$http.defaults.headers.common['Authorization'] = token;
+      const res = await this.$http.get('users?pagenum=1&pagesize=10');
+      const data = res.data;
+      const { meta: { status, msg } } = data;
+      if (status === 200) {
+        const { data: { users } } = data;
+        this.list = users;
+      } else {
+        this.$message.error(msg);
+      }
+    }
+  }
 };
 </script>
 
 <style>
-
+.box-card {
+  height: 100%;
+}
+.searchArea {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.searchArea .searchInput {
+  width: 350px;
+}
 </style>
