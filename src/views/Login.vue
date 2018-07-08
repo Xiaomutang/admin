24bfc55839c7cbb94b0f133c20ea5363c6d66bc0
wangdong <1029507088@qq.com>
2018-07-08 11:09:14 +0800

    登录也布局结束

---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login-wrap">
+    <el-form class="login-from" :label-position="top" label-width="80px" :model="formData">
+      <h2>用户登录</h2>
+      <el-form-item label="名称">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-button class="btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+};
+</script>
+
+<style>
+  .btn {
+    width: 100%;
+  }
+  .login-wrap {
+    background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-wrap .login-from {
+    background-color: #fff;
+    width: 400px;
+    padding: 30px;
+    border-radius: 5px;
+  }
+
+  .login-wrap .login-from .login-btn {
+    width: 100%;
+  }
+</style>
