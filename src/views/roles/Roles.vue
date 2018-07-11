6dca6a2ecfbac03e5415b5c900919cdeb36bb612
wangdong <1029507088@qq.com>
2018-07-11 19:42:23 +0800

    角色列表初步显示

--- src/views/roles/Roles.vue
+++ src/views/roles/Roles.vue
@@ -1,16 +1,74 @@
 <template>
-  <el-card>
+  <el-card class="box-card">
     <my-breadcrumb level1="权限管理" level2="角色列表"></my-breadcrumb>
-
+    <el-row class="row-add">
+      <el-col :span="24">
+        <el-button>添加角色</el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      v-loading="loading"
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="roleName"
+        label="角色名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="roleDesc"
+        label="角色描述"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button plain size="mini" type="primary" icon="el-icon-edit" ></el-button>
+          <el-button plain size="mini" type="danger" icon="el-icon-delete" ></el-button>
+          <el-button @click="handleShowRightsDialog(scope.row)" plain size="mini" type="success" icon="el-icon-check" ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: [],
+      loading: true
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      this.loading = true;
+      const res = await this.$http.get('roles');
+      this.loading = false;
+      const { meta: { status, msg } } = res.data;
+      if (status === 200) {
+        this.list = res.data.data;
+      } else {
+        this.$message.error(msg);
+      }
+    }
+  }
 };
 </script>
 
 <style>
-
+  .box-card {
+    height: 98%;
+    overflow: auto;
+  }
+  .row-add {
+    margin-top: 10px;
+  }
 </style>
